d37a8c8bf02e36d6339f5ae893e97016efd07aaf
HungVuIT <67231540+HungVuIT@users.noreply.github.com>
2021-11-30 02:10:37 +0700

    add

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,9 @@
     <!-- Navbar -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="landingPage.php">
-            Carworld
+        <a class="navbar-brand" href="index.php">
+            CAR WORLD
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -21,21 +20,29 @@
                 </li>
 
             </ul>
-            
-            <?php 
+
+            <?php
             if (!isset($_SESSION["id"])) { ?>
-            
-            <a href="login.php" class="button" id="in">Sign in</a>
-            <a href="register.php" class="button" id="up">Sign up</a>
-            
-            <?php 
-            } 
-            else { ?>
-            <a href="userCart.php" class="button" id="in">Your Cart</a>
-            <a href="profile.php" class="button" id="up">Profile</a>
-            
+
+                <a href="login.php" class="button" id="in">Sign in</a>
+                <a href="register.php" class="button" id="up">Sign up</a>
+
+            <?php
+            } else { ?>
+                <?php
+                if (isset($_SESSION["is_admin"])) { ?>
+                    <a href="admin/admin/dashboard.php" class="button" id="in">Admin</a>
+                    <a href="logout.php" class="button" id="up" >Logout</a>
+                <?php
+                } else { ?>
+                    <a href="userCart.php" class="button" id="in">Your Cart</a>
+                    <a href="profile.php" class="button" id="up">Profile</a>
+                    <a href="logout.php" class="button" id="up" >Logout</a>
+                <?php
+                } ?>
+
             <?php
             } ?>
-        
+
         </div>
     </nav>
