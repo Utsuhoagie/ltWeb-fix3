1041895f0729d1c24deab8d63cebc8443dd9c880
UtsuhoagieRyeuji <thiendangssb@gmail.com>
2021-11-29 22:23:19 +0700

    fix db (uncomment to RESET db)

--- db/SQL/create_db_tables.sql
+++ db/SQL/create_db_tables.sql
@@ -1,3 +1,8 @@
+
+-- Uncomment to reset database!
+-- DROP DATABASE `database`;
+
+
 CREATE DATABASE `database`;
 USE `database`;
 
@@ -5,16 +10,6 @@ SET time_zone = "+07:00";   -- VN time
 
 
 -- Tables without Foreign Keys
-
-CREATE TABLE `tbladmin` (
-  `id` int(11) NOT NULL,
-  `AdminUserName` varchar(255) DEFAULT NULL,
-  `AdminPassword` varchar(255) DEFAULT NULL,
-  `AdminEmailId` varchar(255) DEFAULT NULL,
-  `userType` int(11) DEFAULT NULL,
-  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Contact` (
     `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -24,15 +19,27 @@ CREATE TABLE `Contact` (
    PRIMARY KEY(`id`)
 );
 
-CREATE TABLE `user` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(15) DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
-  `img_path` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `about` (
+    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `address` varchar(200) DEFAULT NULL,
+    `email` varchar(200) DEFAULT NULL,
+    `phone` varchar(200) DEFAULT NULL,
+    `description` varchar(200) DEFAULT NULL,
+    `value` varchar(200) DEFAULT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `User` (
+    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `is_admin` int(1) NOT NULL DEFAULT 0,
+    `name` varchar(30) NOT NULL,
+    `password` varchar(32) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `phone` varchar(15) DEFAULT NULL,
+    `birthday` date DEFAULT NULL,
+    `img_path` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
 
 
 CREATE TABLE `Car` (
@@ -53,32 +60,58 @@ CREATE TABLE `Car` (
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `about` (
-`id` int(11) ,
-`address` varchar(200) DEFAULT NULL,
-`email` varchar(200) DEFAULT NULL,
-`phone` varchar(200) DEFAULT NULL,
-`description` varchar(200) DEFAULT NULL,
-`value` varchar(200) DEFAULT NULL,
-PRIMARY KEY(`id`)
+
+-- News
+
+CREATE TABLE `tblcategory` (
+    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `CategoryName` varchar(200) DEFAULT NULL,
+    `Description` mediumtext DEFAULT NULL,
+    `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+    `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+    `Is_Active` int(1) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `tblsubcategory` (
+    `SubCategoryId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `CategoryId` int(11) DEFAULT NULL,
+    `Subcategory` varchar(255) DEFAULT NULL,
+    `SubCatDescription` mediumtext DEFAULT NULL,
+    `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+    `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+    `Is_Active` int(1) DEFAULT NULL,
+    PRIMARY KEY (`SubCategoryId`)
+);
 
 CREATE TABLE `tblposts` (
-  `id` int(11) NOT NULL,
-  `PostTitle` longtext DEFAULT NULL,
-  `CategoryId` int(11) DEFAULT NULL,
-  `SubCategoryId` int(11) DEFAULT NULL,
-  `PostDetails` longtext CHARACTER SET utf8 DEFAULT NULL,
-  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `Is_Active` int(1) DEFAULT NULL,
-  `PostUrl` mediumtext DEFAULT NULL,
-  `PostImage` varchar(255) DEFAULT NULL,
-  `viewCounter` int(11) DEFAULT NULL,
-  `postedBy` varchar(255) DEFAULT NULL,
-  `lastUpdatedBy` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `PostTitle` longtext DEFAULT NULL,
+    `CategoryId` int(11) DEFAULT NULL,
+    `SubCategoryId` int(11) DEFAULT NULL,
+    `PostDetails` longtext CHARACTER SET utf8 DEFAULT NULL,
+    `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+    `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+    `Is_Active` int(1) DEFAULT NULL,
+    `PostUrl` mediumtext DEFAULT NULL,
+    `PostImage` varchar(255) DEFAULT NULL,
+    `viewCounter` int(11) DEFAULT NULL,
+    `postedBy` varchar(255) DEFAULT NULL,
+    `lastUpdatedBy` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tblcomments` (
+    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `postId` int(11) DEFAULT NULL,
+    `name` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `comment` mediumtext DEFAULT NULL,
+    `postingDate` timestamp NULL DEFAULT current_timestamp(),
+    `img_path` varchar(255) NOT NULL,
+    `status` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
 
 
@@ -93,15 +126,8 @@ CREATE TABLE `Order` (
     PRIMARY KEY (`user_id`, `car_id`)
 );
 
-
--- CREATE TABLE `CarImg` (
---     `car_id` int(11) UNSIGNED NOT NULL,
---     `car_img_path` varchar(255) NOT NULL UNIQUE,
---     FOREIGN KEY (`car_id`) REFERENCES `Car`(`id`),
---     PRIMARY KEY (`car_img_path`)
--- );
-
 CREATE TABLE `CarReview` (
+    `review_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` int(11) UNSIGNED NOT NULL,
     `car_id` int(11) UNSIGNED NOT NULL,
     `review` varchar(256) NOT NULL,
@@ -109,114 +135,5 @@ CREATE TABLE `CarReview` (
     `date_posted` datetime,
     FOREIGN KEY (`user_id`) REFERENCES `User`(`id`),
     FOREIGN KEY (`car_id`) REFERENCES `Car`(`id`),
-    PRIMARY KEY (`user_id`, `car_id`)
+    PRIMARY KEY (`review_id`)
 );
-
-
-CREATE TABLE `tblcategory` (
-  `id` int(11) NOT NULL,
-  `CategoryName` varchar(200) DEFAULT NULL,
-  `Description` mediumtext DEFAULT NULL,
-  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `Is_Active` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `tblsubcategory` (
-  `SubCategoryId` int(11) NOT NULL,
-  `CategoryId` int(11) DEFAULT NULL,
-  `Subcategory` varchar(255) DEFAULT NULL,
-  `SubCatDescription` mediumtext DEFAULT NULL,
-  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `Is_Active` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `tblcomments` (
-  `id` int(11) NOT NULL,
-  `postId` int(11) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `comment` mediumtext DEFAULT NULL,
-  `postingDate` timestamp NULL DEFAULT current_timestamp(),
-  `img_path` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `tbladmin`
---
-ALTER TABLE `tbladmin`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `AdminUserName` (`AdminUserName`);
-
---
--- Chỉ mục cho bảng `tblcategory`
---
-ALTER TABLE `tblcategory`
-  ADD PRIMARY KEY (`id`);
-
---
--- Chỉ mục cho bảng `tblcomments`
---
-ALTER TABLE `tblcomments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`),
-  ADD KEY `postId` (`postId`);
-
---
--- Chỉ mục cho bảng `tblposts`
---
-ALTER TABLE `tblposts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`),
-  ADD KEY `postcatid` (`CategoryId`),
-  ADD KEY `postsucatid` (`SubCategoryId`),
-  ADD KEY `subadmin` (`postedBy`);
-
---
--- Chỉ mục cho bảng `tblsubcategory`
---
-ALTER TABLE `tblsubcategory`
-  ADD PRIMARY KEY (`SubCategoryId`),
-  ADD KEY `catid` (`CategoryId`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `tbladmin`
---
-ALTER TABLE `tbladmin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT cho bảng `tblcategory`
---
-ALTER TABLE `tblcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT cho bảng `tblcomments`
---
-ALTER TABLE `tblcomments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT cho bảng `tblposts`
---
-ALTER TABLE `tblposts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT cho bảng `tblsubcategory`
---
-ALTER TABLE `tblsubcategory`
-  MODIFY `SubCategoryId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
--- COMMIT;
