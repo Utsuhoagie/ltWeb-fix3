016fa14a5a25d2ed7ddf2eca5f2558138837e87c
HuuHanh31 <91646453+HuuHanh31@users.noreply.github.com>
2021-11-29 23:00:05 +0700

    Update login.php

--- login.php
+++ login.php
@@ -25,14 +25,20 @@
 	<link rel = "icon" href = "img/logo.png" type = "image/x-icon">
 
 	<!-- Bootstrap core CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	
   	<!-- Custom styles -->
-  	<link rel="stylesheet" href="css/base.css">
+  	<link rel="stylesheet" href="css/both.css">
 	<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="css/navbar.css">
 </head>
 <body>
+	<!-- Navigation -->
+	<?php
+		include "includes/navbar.php"
+	?>
 	<div class="container">	
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4 well">
@@ -55,7 +61,7 @@
 				<span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" id="row1">
 			<div class="col-md-4 col-md-offset-4 text-center">	
 			<span id="check">New User? </span><a href="register.php">Sign Up Here</a>
 			</div>
