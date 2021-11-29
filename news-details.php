bc8c8b0d36745bca36927deb895b6bca574a5347
HuuHanh31 <91646453+HuuHanh31@users.noreply.github.com>
2021-11-29 22:59:41 +0700

    Update news-details.php

--- news-details.php
+++ news-details.php
@@ -28,7 +28,7 @@
   <title>Carworld - News</title>
 
   <!-- Logo -->
-  <link rel = "icon" href = "img/favicon.png" type = "image/x-icon">
+  <link rel = "icon" href = "img/Hanh/favicon.png" type = "image/x-icon">
 
   <!-- Bootstrap core CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
